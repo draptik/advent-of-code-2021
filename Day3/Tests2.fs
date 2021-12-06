675197a2d76cd82506ac69f0e5c075843a8d0493
Patrick Drechsler <patrick@pdrechsler.de>
2021-12-06 21:41:50 +0100

    day 3 - part 2 -> prepare test setup for demo data

--- Day3/Tests2.fs
+++ Day3/Tests2.fs
@@ -11,6 +11,8 @@ let inputData = System.IO.File.ReadAllLines(input) |> Array.toList
 (* Types ============================================================================================================ *)
 type Bit = Zero | One
 type Bits = Bit seq
+type RowBits = Bits
+type ColumnBits = Bits
 type GammaRate = Bits
 type EpsilonRate = Bits
 
@@ -37,13 +39,13 @@ let toDecimal (bits: Bits) : int =
 let getBitAt bits index =
     bits |> Seq.item index
 
-let toBitsColumn (bits: Bits seq) (index: int) : Bits =
+let toBitsColumn (bits: Bits seq) (index: int) : ColumnBits =
     bits |> Seq.map (fun bs -> getBitAt bs index)
 
 let getBitsWidth bss =
     bss |> Seq.head |> Seq.length
 
-let getMostCommonValue (bits: Bits) : Bit =
+let getMostCommonValue (bits: ColumnBits) : Bit =
     let numberOfOnes = bits |> Seq.filter (fun b -> b = One) |> Seq.length
     let numberOfZeros = (bits |> Seq.length) - numberOfOnes
     if numberOfOnes >= numberOfZeros then One
@@ -54,13 +56,14 @@ let getGammaRate (bss: Bits seq) : GammaRate =
     |> List.map (toBitsColumn bss)
     |> List.map getMostCommonValue
     |> Seq.ofList
-    
-let invertBits bs =
-    bs
-    |> Seq.map (fun b ->
-        match b with
+
+let invertBit b =
+    match b with
         | One -> Zero
-        | Zero -> One)
+        | Zero -> One
+        
+let invertBits bs =
+    bs |> Seq.map invertBit
 
 let getEpsilonRate gammaRate : EpsilonRate =
     gammaRate |> invertBits
@@ -83,4 +86,46 @@ let ``day3 - part 1 with types`` () =
     let result = calcResult gammaRate epsilonRate
     let expectedResult = 3813416
     result =! expectedResult
+
+let rawDemo = [
+        "00100"
+        "11110"
+        "10110"
+        "10111"
+        "10101"
+        "01111"
+        "00111"
+        "11100"
+        "10000"
+        "11001"
+        "00010"
+        "01010"
+    ]
+
+type OxygenGeneratorRating = Bits
+type C02ScrubberRating = Bits
+
+let getLeastCommonValue (bits: ColumnBits) : Bit =
+    bits |> getMostCommonValue |> invertBit
     
+let getOxygenGeneratorRating bss : OxygenGeneratorRating =
+    // TODO We need a recursive function here
+    let initialColumnFirstBit = getMostCommonValue  
+    
+    [One; Zero; One; One; One] |> Seq.ofList
+
+let getCO2ScrubberRating bss : C02ScrubberRating =
+    // TODO 01010
+    [Zero; One; Zero; One; Zero] |> Seq.ofList
+    
+[<Fact>]
+let ``day 3 - part 2 experiments`` () =
+    let allData = rawDemo |> List.map toBits
+    
+    let oxygenGeneratorRating = getOxygenGeneratorRating allData
+    let expectedOxygenGeneratorRating = 23
+    (oxygenGeneratorRating |> toDecimal) =! expectedOxygenGeneratorRating
+    
+    let co2ScrubberRating = getCO2ScrubberRating allData
+    let expectedCO2ScrubberRating = 10
+    (co2ScrubberRating |> toDecimal) =! expectedCO2ScrubberRating
