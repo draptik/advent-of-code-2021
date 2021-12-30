c05925ebfd73dc66e2c2dcfb2872b1ddd705d477
Patrick Drechsler <patrick@pdrechsler.de>
2021-12-31 00:26:54 +0100

    day 6 - part 2 - started profiling..

--- Day6/Part2.fs
+++ Day6/Part2.fs
@@ -16,11 +16,23 @@ let nextDay state : State * Action =
 
 let getStateAction states = states |> Seq.map nextDay
 
-(* TODO After profiling, this function takes up all the time *)
+let getAction stateAction : Action =
+    let _, a = stateAction
+    a
+    
+let isSpawn (stateAction: State * Action) : bool =
+    let a = getAction stateAction
+    a = Spawn
+
+(* TODO After profiling, this function takes up all the time 2/2 *)
+let getLength xs =
+    xs |> Seq.length
+    
+(* TODO After profiling, this function takes up all the time 1/2 *)
 let getNumberOfSpawns stateAction =
     stateAction
-    |> Seq.filter (fun (_,a) -> a = Spawn)
-    |> Seq.length
+    |> Seq.filter isSpawn
+    |> getLength
 
 let getNewFish numberOfSpawns newLanternFishState = Seq.replicate numberOfSpawns newLanternFishState
 
@@ -38,8 +50,7 @@ let rec getStatesForDays days states : States =
         states
     else
         let days' = days - 1
-        let states' = getStatesForNextDay states
-        getStatesForDays days' states'
+        getStatesForDays days' (getStatesForNextDay states)
     
 let getNumberOfFishes (states:States) : int64 =
     states |> Seq.fold (fun acc _ -> acc + 1L) (0L)
