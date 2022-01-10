f75090d760221c55743a6fcd46e65abc368f23c4
Patrick Drechsler <patrick@pdrechsler.de>
2022-01-10 16:22:43 +0100

    day 7 part2 - replace list.fold with sumOfRange

--- Day7/Part2.fs
+++ Day7/Part2.fs
@@ -5,15 +5,10 @@ open Day7.Part1
 (*  As it turns out, crab submarine engines don't burn fuel at a constant rate.
     Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last:
     the first step costs 1, the second step costs 2, the third step costs 3, and so on.
-    
-    This works, but is slow (~30sec)...
      *)
 let calcFuel' (crabPosition:CrabPosition) (targetPosition:TargetPosition) : Fuel =
     let distance = (crabPosition - targetPosition) |> abs
-    let result =
-        List.fold (fun acc elem -> acc + elem)
-            0
-            [0..distance]
+    let result = distance * (distance + 1) / 2
     result
 
 let getFuelUsageAtBestPosition' (crabPositions:CrabPosition list) : Fuel =
